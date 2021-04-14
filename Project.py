8ec0a095ea21f1af8383f19f56059f7fbfe58ae1
JackLuu272 <78498034+JackLuu272@users.noreply.github.com>
2021-04-14 09:36:32 -0700

    Program is delayed

--- Project.py
+++ Project.py
@@ -5,28 +5,34 @@ from PIL import ImageOps
 from numpy import *
 
 class cord():
-    replay = (684,402)
-    dino = (126,412)
-#252,414 cord box
+    replay = (674,477)
+    dino = (430,483)
+    # box 449,512
+
 def reset():
     pyautogui.click(cord.replay)
     print("The game will be replayed.")
 
 def spce():
-    time.sleep(0.1)
     pyautogui.keyDown('space')
+    time.sleep(0.1)
+    pyautogui.keyUp('space')
+    time.sleep(0.1)
 
-def dinosa():
-    fossil = (cord.dino[0]+252, cord.dino[1], cord.dino[1]+292, cord.dino[0]+2)
+def dinosa(): 
+    fossil = (cord.dino[0]+ 19, cord.dino[1], cord.dino[1]+ 3, cord.dino[0]+29)
     sample = ImageGrab.grab(fossil)
     detect = ImageOps.grayscale(sample)
-    x = array(detect.getcolors())
-    print(x)
+    a = array(detect.getcolors())
+    print(a.sum())         
 
-time.sleep(5)
+time.sleep(4.5)
 reset()    
 
 while True:
     dinosa()
-                                               
-    
+    #if (dinosa() != ):
+        #spce()
+        #time.sleep(0.001)
+
+                                    
